0e75c5579f110610a00e8372aa3f5e3ba004c9c6
karanrana <karanrana3095@gmail.com>
2024-02-28 22:38:16 +0530

    finalcommit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="bg-white dark:bg-white fixed w-full z-20 top-0 mb-10 border-b border-gray-200 dark:border-gray-600">
+      <nav className="bg-white dark:bg-white fixed w-full z-20 top-0 mb-10 border-b border-gray-200 ">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://letscodejobs.in/wp-content/uploads/2024/02/cropped-cropped-logo2-3.png" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://letscodejobs.in/wp-content/uploads/2024/02/cropped-cropped-logo2-3.png" className="h-8" alt="Flowbite Logo" />
@@ -71,7 +71,7 @@ const Navbar = () => {
             </button>
           </div>
           <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? '' : 'hidden'}`} id="navbar-sticky">
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
               <li>
                <Link to="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
               </li>
