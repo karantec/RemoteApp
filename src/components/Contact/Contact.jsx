72ee1d911144610e5f0ee0a5741eb8047f1f6017
karanrana <karanrana3095@gmail.com>
2024-03-10 00:42:05 +0530

    finalcommit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,9 @@
 import Navbar from "../Navbar/Navbar"
 import Footer from "../Footer/Footer"
-import ContactHead from "./ContactHead"
 import ContactContent from "./ContactContent"
 const Contact = () => {
   return (
     <div><Navbar/>
-            <ContactHead/>
             <ContactContent/>
      <Footer/>
      </div>
