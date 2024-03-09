281109f099d9fe06877d36bf8200cb3546f21372
karanrana <karanrana3095@gmail.com>
2024-03-10 00:37:06 +0530

    finalcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-// import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { useEffect, useLayoutEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Log/Login";
 import Signup from "./components/Log/Signup";
@@ -7,11 +7,26 @@ import Resource from "./components/Resources/Resource";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Service from "./components/Services/Services";
-const App = () => {
-  
 
+// ScrollToTop component for scrolling to the top when route changes
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  // This effect will run whenever the pathname changes
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0); // Scrolls to the top of the page
+  }, [pathname]);
+
+  // Returning null because this component doesn't render anything
+  return null;
+};
+
+const App = () => {
   return (
     <div>
+      {/* Include the ScrollToTop component */}
+      <ScrollToTop />
+
       <Routes  basename="/">
         <Route  path="/" element={<Home />} />
         <Route   path="/Login" element={<Login/>}></Route>
