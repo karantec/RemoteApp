ef1612ce568354e7b1837eb34219354567ee9f61
karanrana <karanrana3095@gmail.com>
2024-03-09 12:03:45 +0530

    Finalcommit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
           <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? '' : 'hidden'}`} id="navbar-sticky">
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 ">
               <li>
-               <Link to="/" className="block py-2 px-3 text-white  rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" >Home</Link>
+               <Link to="/" className="block py-2 px-3 text-black-500  rounded md:bg-transparent  md:p-0 md:dark:text-blue-500" >Home</Link>
               </li>
               <li>
               <Link to ="/About" className="block py-2 px-3 text-black  rounded md:bg-transparent  md:p-0  ">About</Link>
